c0819ee5d8de982c2024a72cc316cca90b40f7e5
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2024-07-02 14:13:15 -0400

    404 Error page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ejsMate = require('ejs-mate');
 const path = require('path');
 const methodOverride = require('method-override');
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 // |----------------| Require Libraries |----------------|//
 
 // Require Models
@@ -54,6 +55,8 @@ app.get('/campgrounds/new', (req, res) => {
 
 // === Campgrounds List Page (POST) === //
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
+    if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -85,8 +88,15 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     res.redirect('/campgrounds');
 }));
 
+// === 404 Page === //
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page not Found', 404));
+});
+
 // === Error Handler === //
 app.use((err, req, res, next) => {
+    const { statusCode = 500, message = 'Something went wrong' } = err;
+    res.status(statusCode).send(message);
     res.send('OH NO! SOMETHING WENT WRONG :(');
 });
 // |------------------| Website Routes |------------------| //
