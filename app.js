17267315dded66d32272a0c45eff151f76157e8b
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2024-07-04 11:28:48 -0400

    Displaying reviews for each campground.

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) =
 
 // === Campground Details Page === //
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
+    const campground = await Campground.findById(req.params.id).populate('reviews');
     res.render('campgrounds/show', { campground });
 }));
 
