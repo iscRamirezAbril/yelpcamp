8de12ec249db52552e35706efbcde67759b9bd4f
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2024-07-10 11:20:02 -0400

    Adding error flash alerts

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -46,12 +46,20 @@ router.post('/', validateCampground, catchAsync(async (req, res, next) => {
 // === Campground Details Page === //
 router.get('/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
+    if (!campground) {
+        req.flash('error', 'The campground was not found!');
+        return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/show', { campground });
 }));
 
 // === Campground Edit Page (GET) === //
 router.get('/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
+    if (!campground) {
+        req.flash('error', 'The campground was not found!');
+        return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/edit', { campground });
 }));
 
