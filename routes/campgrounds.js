cef76a61b91162535a3da2edbc62f9879e7217f7
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2024-07-10 11:15:04 -0400

    Adding Style to flash messages

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -59,6 +59,7 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
 router.put('/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    req.flash('success', 'The campground was updated successfully!');
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
@@ -66,6 +67,7 @@ router.put('/:id', validateCampground, catchAsync(async (req, res) => {
 router.delete('/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted campground!');
     res.redirect('/campgrounds');
 }));
 
