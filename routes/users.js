fe9b314cfc45a4a22d10e2cba1902a076564412e
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2024-07-11 11:44:49 -0400

    Fixing Register Route

--- routes/users.js
+++ routes/users.js
@@ -15,13 +15,16 @@ router.get('/register', (req, res) => {
 });
 
 // === Register user page (POST) === //
-router.post('/register', catchAsync(async (req, res) => {
+router.post('/register', catchAsync(async (req, res, next) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registerUser = await User.register(user, password);
-        req.flash('success', 'Welcome to Yelpcamp!');
-        res.redirect('/campgrounds');
+        req.login(registerUser, err => {
+            if(err) return next();
+            req.flash('success', 'Welcome to Yelpcamp!');
+            res.redirect('/campgrounds');
+        });
     } catch(e) {
         req.flash('error', e.message);
         res.redirect('/register');
