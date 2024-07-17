317b7ae30577386233f9c53accbea0c4fc980e17
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2024-07-16 19:57:32 -0700

    Security policy added!

--- app.js
+++ app.js
@@ -53,6 +53,49 @@ app.use(mongoSanitize({
 app.use(helmet());
 // |------------------| Uses for the app |----------------| //
 
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+    "https://cdn.maptiler.com/",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+    "https://cdn.maptiler.com/",
+];
+const connectSrcUrls = [
+    "https://api.maptiler.com/",
+];
+
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dkpjoasyn/",
+                "https://images.unsplash.com/",
+                "https://api.maptiler.com/",
+        	    "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
 // |--------------| Sessions Configurations |-------------| //
 const sessionConfig = {
     name: 'session',
